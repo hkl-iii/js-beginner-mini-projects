d82eb4f7dca211747bf9f917bd2fe30d5717a54a
unknown <rahleggon@gmail.com>
2021-04-11 21:37:04 -0500

    added function clock() over everything in js file. Then change all const to let so that the clock can run.

--- digital-clock/app.js
+++ digital-clock/app.js
@@ -1,8 +1,23 @@
-const fullDate = new Date();
-const hours = fullDate.getHours();
-const mins = fullDate.getMinutes();
-const secs = fullDate.getSeconds();
+function clock() {
+let fullDate = new Date();
+let hours = fullDate.getHours();
+let mins = fullDate.getMinutes();
+let secs = fullDate.getSeconds();
 
+if (hours < 10) {
+    hours = "0" + hours;
+}
+if (mins < 10) {
+    mins = "0" + mins;
+}
+if (secs < 10) {
+    secs = "0" + secs;
+}
 document.getElementById('hour').innerHTML = hours;
 document.getElementById('minute').innerHTML = mins;
 document.getElementById('second').innerHTML = secs;
+
+}
+
+setInterval(clock,100);
+
