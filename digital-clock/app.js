6fd2c41a3c02c6514fbad7c91b8f3062e34411d0
unknown <rahleggon@gmail.com>
2021-04-11 21:15:44 -0500

    changing the clock time to the actual time.

---
+++ digital-clock/app.js
@@ -0,0 +1,8 @@
+const fullDate = new Date();
+const hours = fullDate.getHours();
+const mins = fullDate.getMinutes();
+const secs = fullDate.getSeconds();
+
+document.getElementById('hour').innerHTML = hours;
+document.getElementById('minute').innerHTML = mins;
+document.getElementById('second').innerHTML = secs;
